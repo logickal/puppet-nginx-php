df19e0b7003a2d472cd00bc73a19bfa3db9da32c
Jeremy Dickens <jeremy@offnominal.com>
2013-05-13 09:53:04 -0500

    I don't need the french language pack in my version, removing

--- manifests/init.pp
+++ manifests/init.pp
@@ -29,6 +29,6 @@ class nginxphp () {
   }
   
   # install base packages
-  $sysPackages = ["build-essential", "curl", "language-pack-fr", "language-pack-en"]
+  $sysPackages = ["build-essential", "curl", "language-pack-en"]
   package { $sysPackages: ensure => "latest", }
 }
